0bac99d88017c99c8ae7b1dfd9aa49fe4c38a588
4ndr3sxy <andrescastanedaxy@gmail.com>
2020-12-30 10:20:31 -0500

    big change in logic.js - oscilation in rope functional

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('canvasCuerda')
 var output = document.getElementById('output')
 var ctx = canvas.getContext('2d')
-var size = 2000
+var size = 4000
 var sizeRope = 1500
 var mousePos
 
@@ -14,29 +14,43 @@ function load() {
 
 //draw in canvas
 function draw(mX, mY, lX, lY) {
+    //resetCanvas()
     ctx.beginPath()
     ctx.strokeStyle = "#955F53"
     ctx.moveTo(mX, mY)
     ctx.lineTo(lX, lY)
     ctx.stroke()
+    
+    /*
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    */
 }
 
 var xTmp, yTmp
+var valRgb = false
 //event move mouse
 canvas.addEventListener("mousemove", function (evt) {
     //showCoor(output, mousePos.x, mousePos.y)
     mousePos = posMouseF(canvas, evt)
-    
-    if(validateLimitCoor){
-        console.log("validate entry")
-        xTmp = mousePos.x
-        yTmp = mousePos.y
-        drawParabolic()
-        //setTimeout(test123,10)
-    }else{
-        reDraw()
+    var rgb = !valRgb ? getaverageColor() : true
+    if (rgb){
+        if(validateLimitCoor){
+            valRgb = false
+            console.log("validate entry")
+            xTmp = mousePos.x
+            yTmp = mousePos.y
+            //drawParabolic()
+            
+            drawParabolic()
+            //setTimeout(drawParabolic,1)
+        }else{
+            reDraw()
+        }
     }
 }, false)
+
 
 //get coordinates of mouse in X and Y 
 function posMouseF(canvas, evt) {
@@ -88,56 +102,169 @@ function getHypotenuse(){
 
 
 canvas.addEventListener("click", function () {
-    reDraw()
+    let adjMousePosX = mousePos.x
+    let adjMousePosY = mousePos.y
+    adjMousePosX -= 2000
     console.log("Dio click"+ mousePos.x + " - "+mousePos.y)
+    //console.log(Math.abs(getGrades(adjMousePosX,adjMousePosY) - 90))
+    
+    //reDraw()
 }, false)
 
 var tmpCoor = 1
 var timer
+var type = ""
+var gradesRest = 0
 function drawParabolic(){
-    if(xTmp > 1000){
+    if(xTmp > size/2){
+        type = "D"
         parabolicRightLeft()
     }else{
+        type ="I"
         parabolicLeftRight()
-    }
-}
-
-function parabolicLeftRight(){
-    resetCanvas()
-    draw(size / 2, 0,xTmp, yTmp)
-    if(tmpP < 1000){
-        timer = setTimeout(parabolicRightLeft,100)
-    }else{
-         clearTimeout(timer)
-         tmpP = 0
     }
 }
 
 var valYTmp = false
 var dParabolicX = 0
 var tmpP = 0
+var contRL = 0
+var gradesCoor = 0
+var valGrades = 0
+var valParR = 0
 function parabolicRightLeft(){
+    console.log("**"+gradesRest+"**")
     resetCanvas()
-    draw(size / 2, 0,xTmp, yTmp)
-    xTmp--
-    if(yTmp<=1500 && !valYTmp){
-        if(xTmp % 5 == 0){
-            yTmp++
+    if(type == "D"){
+        if(valGrades == 0){
+            let adjMousePosX = mousePos.x
+            let adjMousePosY = mousePos.y
+            adjMousePosX -= 2000
+            gradesCoor = getGrades(adjMousePosX,adjMousePosY)
+            gradesRest =parseInt(((90 - gradesCoor) / 3)*2)
+            console.log(gradesRest+"--")
+            valGrades++
+        }
+        var ap = (Math.PI / 180) * parseInt(gradesCoor);
+        var Xap = (size/2) + sizeRope * Math.cos(ap);
+        var Yap = 0 + sizeRope * Math.sin(ap);
+        draw(size / 2, 0,Xap, Yap)
+        gradesCoor+=3
+        console.log(gradesCoor+"**")
+        
+        
+        if(gradesCoor > (gradesRest + 90)){
+            //type="I"
+            console.log("go to left-right")
+            parabolicLeftRight()
+        }else{
+            setTimeout(parabolicRightLeft,1)
         }
     }else{
-        if(xTmp % 5 == 0){
-            yTmp-- 
+        if(valParR==0){
+            //gradesRest =parseInt(((90 - gradesCoor) / 3)*2)
+            gradesRest = parseInt((gradesRest/3)*2)
+            //gradesRest = 
+            valParR++
         }
-        valYTmp = true
+        var ap = (Math.PI / 180) * parseInt(gradesCoor);
+        var Xap = (size/2) + sizeRope * Math.cos(ap);
+        var Yap = 0 + sizeRope * Math.sin(ap);
+        draw(size / 2, 0,Xap, Yap)
+        gradesCoor+=3
+        if(gradesCoor > (gradesRest + 90)){
+            valParR=0
+            if(gradesRest == 0){
+                resetCanvas()
+                draw(size / 2, 0, size / 2, sizeRope)
+                valRgb = false
+                validateLimitCoor = false
+                valGrades = 0
+                //clearInterval(timerP)
+                
+            }else{
+                parabolicLeftRight()
+            }
+        }else{
+            setTimeout(parabolicRightLeft,1)
+        }
     }
-    if(tmpP < 1000){
-        timer = setTimeout(parabolicRightLeft,100)
+}
+
+var valParL = 0
+function parabolicLeftRight(){
+    console.log("**"+gradesRest+"**")
+    resetCanvas()
+    if(type == "I"){
+        if(valGrades == 0){
+            let adjMousePosX = mousePos.x
+            let adjMousePosY = mousePos.y
+            adjMousePosX -= 2000
+            gradesCoor = getGrades(adjMousePosX,adjMousePosY)
+            gradesRest =Math.abs(parseInt(((90 - gradesCoor) / 3)*2))
+            valGrades++
+        }
+        var ap = (Math.PI / 180) * parseInt(gradesCoor);
+        var Xap = (size/2) + sizeRope * Math.cos(ap);
+        var Yap = 0 + sizeRope * Math.sin(ap);
+        draw(size / 2, 0,Xap, Yap)
+        gradesCoor-=3
+        if(gradesCoor < (90 - gradesRest)){
+            //type="D"
+            parabolicRightLeft()
+        }else{
+            setTimeout(parabolicLeftRight,1)
+        }
     }else{
-         clearTimeout(timer)
+        if(valParL==0){
+            //gradesRest =parseInt(((90 - gradesCoor) / 3)*2)
+            gradesRest = parseInt((gradesRest/3)*2)
+            //gradesRest = 
+            valParL++
+        }
+        var ap = (Math.PI / 180) * parseInt(gradesCoor);
+        var Xap = (size/2) + sizeRope * Math.cos(ap);
+        var Yap = 0 + sizeRope * Math.sin(ap);
+        draw(size / 2, 0,Xap, Yap)
+        gradesCoor-=3
+        if(gradesCoor < (90 - gradesRest)){
+            valParL=0
+            if(gradesRest == 0){
+                resetCanvas()
+                draw(size / 2, 0, size / 2, sizeRope)
+                //clearInterval(timerP)
+                valRgb = false
+                validateLimitCoor = false
+                valGrades = 0
+            }else{
+                parabolicRightLeft()
+            }
+        }else{
+            setTimeout(parabolicLeftRight,1)
+        }
     }
-    tmpP++
 }
 
 function diferentParabolicX(){
 
+}
+
+function getaverageColor() {
+    let imageData, data, j 
+    imageData = ctx.getImageData(mousePos.x, mousePos.y, mousePos.x, mousePos.y)
+    data = imageData.data
+    //console.log(data[0])
+    if(data[0] == 149 || data[0] == 148){
+        valRgb = true
+        return true
+    }
+    return false
+}   
+
+function getGrades(x,y){
+    let grades = Math.atan(x/y)
+    grades = grades * 180 / Math.PI
+    grades = Math.abs(grades -90)
+    //console.log("**"+grades)
+    return grades
 }
